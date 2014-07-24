339f5178805bbf43ccfa9aaa7429d027d941c75e
Daniel Sanchez <dsan@mit.edu>
2014-07-24 00:24:40 -0700

    phoenix icon animation works on hover

--- ndapp/public/javascripts/controllers/NavBarController.js
+++ ndapp/public/javascripts/controllers/NavBarController.js
@@ -59,36 +59,58 @@ ndapp.controller('navbarController', function($scope, ndService) {
     //   }
     // });
 
-    var glow;
-    // (function d3Init() {
-    //   glow = d3.select(".glowContainer")
-    //            .append("svg")
-    //            .attr("class", "iconGlow")
-    //            .attr("width", "120px")
-    //            .attr("height", "120px")
-    //            .append("circle")
-    //            .attr("cx", 60)
-    //            .attr("cy", 60)
-    //            .attr("r", 39)
-    //            .attr("fill", "white");
-    // })();
-    
-    // $(".phoenixIcon").hover(function(){
-    //   $(this).animate({"opacity": "1"}, 150);
-    //   glow.transition()
-    //       .attr("r", 50)
-    //       .duration(500);/*.attr("opacity", 0).duration(1500)*/;
+    var two = new Two({width: 120, height: 120}).appendTo($(".glowContainer")[0]);
+    var r1 = 39, r2 = 50, currentRadius = r1, rDiff = r2 - r1;
+    var circle = two.makeCircle(60, 60, r1);
+    var animating = false;
 
-    //   $(".iconGlow").animate({"opacity": 1}, 100).animate({"opacity": 0}, 400); 
-    //   glow.transition()
-    //       .attr("r", 39)
-    //       .duration(1)
-    //       .delay(500);
+    circle.fill = 'white';
+    circle.noStroke();
 
-    //   $(".iconGlow").animate({"opacity": 1}, 1);
-    // }, function(){
-    //   $(this).animate({"opacity": "0"}, 150);
-    // });
+    two.update();
+
+
+    function animateGlow(frameCount) {
+
+      var totalMs = 500;
+      var timeRatio = totalMs / 1000;
+
+      var totalFrames = 60 * timeRatio;
+
+      if (frameCount / 60 <= timeRatio) {
+
+        currentRadius += rDiff / totalFrames
+        circle.scale = currentRadius / r1;
+
+        var framesBeforeFade = 15;
+        if (frameCount > framesBeforeFade) {
+          circle.opacity -= 1 / (totalFrames - framesBeforeFade);
+        }
+      } else {
+        circle.scale = 1;
+        circle.opacity = 1;
+        currentRadius = r1;
+
+        animating = false;
+        two.frameCount = 0;
+        two.unbind('update');
+        two.update();
+        two.pause();
+      }
+    }
+
+    $(".phoenixIcon").hover(function(){
+      $(this).animate({"opacity": "1"}, 150);
+      if (!animating) {
+        animating = true;
+        
+        two.bind('update', function(frameCount) {
+          animateGlow(frameCount);
+        }).play();
+      }
+    }, function(){
+      $(this).animate({"opacity": "0"}, 150);
+    });
 
     // $(".phoenixIcon").on("click", function(){
     //   glow.transition()
