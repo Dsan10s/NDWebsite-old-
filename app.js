c69991b9ec5f3b118b5f3941c196e0a885fec6ef
Daniel Sanchez <dsan@mit.edu>
2014-08-01 21:26:37 -0700

    listening for heroku port in app

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.configure('production', function(){
 require('./routes/indexRouter')(app);
 require('./routes/dataRouter')(app);
 
-app.listen(3000, function(){
+app.listen(process.env.port || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
