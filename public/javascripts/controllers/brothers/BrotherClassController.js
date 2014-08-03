7b6815556f66466f8f86a2b49aea020fc2741b1e
Daniel Sanchez <dsan@mit.edu>
2014-08-02 19:17:36 -0700

    brother pages have magnification icons

--- public/javascripts/controllers/brothers/BrotherClassController.js
+++ public/javascripts/controllers/brothers/BrotherClassController.js
@@ -4,32 +4,64 @@ ndapp.controller('brotherClassController', function($scope, ndService) {
 
   $scope.viewModel = {
     brothers: {},
-    brotherNames: []
+    brotherNames: [], 
+    brotherIconCenters: {}, 
+    brotherIconSize: undefined,
+    calcIconCenters: function() {}
   }
-
+  var public = $scope.viewModel;
   var setViewModel = function(data) {
-    private.brothers = data.brothers;
-    $scope.viewModel.brotherNames = Object.keys(private.brothers);
+    $scope.$apply(function() {
+      public.brothers = data.brothers;
+      public.brotherNames = Object.keys(public.brothers);
+      public.calcIconCenters = helpers.calcIconCenters;
+    });
+  };
 
-    $scope.$apply();
-  }
-
-  var private = (function() {
-    return {
-      classYear: brotherClassViewVars.classYear, // defined in view
-      brothers: {}, 
-    }
-  })();
+  var private = {
+    classYear: brotherClassViewVars.classYear
+  };
+  var setPrivateVars = function() {
+    
+  };
 
   var helpers = (function() {
+
+    /**
+     * Calculates the position of the center of
+     * an element.
+     */
+    function calcCenter(elem) {
+      var offset = elem.offset();
+      var x = offset.left + (elem.width()/2);
+      var y = offset.top + (elem.height()/2);  
+      return {x: x, y: y};
+    }
+
+    function calcIconCenters() {
+      var iconCenters = {};
+      $(".brotherIcon").each(function(i) {
+        var center = calcCenter($(this));
+        iconCenters["#brotherIcon" + i] = center;
+      });
+      return iconCenters;
+    }
  
-    function sizingJS() {}
+    function sizingJS() {
+      // var iconSize = public.brotherIconSize = $(window).width() / public.brotherNames.length;
+      // $(".brotherIcon").height(iconSize)
+      //                  .width(iconSize)
+      //                  .css("border-radius", (iconSize/2) + "px");
+    }
 
     function responsiveJS() {
       sizingJS();
+      $scope.viewModel.brotherIconCenters = calcIconCenters();
     }
 
     return {
+      calcCenter: calcCenter, 
+      calcIconCenters: calcIconCenters,
       sizingJS: sizingJS, 
       responsiveJS: responsiveJS
     }
@@ -40,18 +72,65 @@ ndapp.controller('brotherClassController', function($scope, ndService) {
     ndService.ajax.brothersDeferred(private.classYear).done(function(brothers) {
       ndService.headerIntroDeferred.resolve(false);
 
+      setPrivateVars();
       setViewModel({brothers: brothers});
 
       helpers.sizingJS();
       $(window).resize(function() {
         helpers.responsiveJS();
       });
+
+      eventHandlers();
     });
     
-
   })();
 
   function eventHandlers() {
-    
+    function resizeIconBullet(e) {
+      $(".brotherIcon").each(function(i) {
+        var center = $scope.viewModel.brotherIconCenters["#brotherIcon" + i];
+        var xDiff = Math.abs(center.x - e.pageX);
+        var maxSizeMultiplier = 2.2;
+        var maxDiff = public.brotherIconSize * 2.5;
+        var maxSizeMultDiff = maxSizeMultiplier - 1;
+
+        var newSize;
+
+        if (xDiff > maxDiff) {
+          newSize = public.brotherIconSize;
+        } else {
+          var multiplier = ((-maxSizeMultDiff/maxDiff)*xDiff) 
+                           + maxSizeMultiplier;
+          newSize = public.brotherIconSize * multiplier;
+        }
+
+        $(this).height(newSize)
+               .width(newSize)
+               .css("border-radius", (newSize/2) + "px");
+      });
+    }
+
+    $("#brotherSelector").on({
+      mouseenter: function() {
+        $(document).on("mousemove", resizeIconBullet);
+      }, 
+      mouseleave: function() {
+        $(document).off("mousemove", resizeIconBullet);
+        $(".brotherIcon").animate({
+          "height": public.brotherIconSize, 
+          "width": public.brotherIconSize, 
+          "border-radius": (public.brotherIconSize/2) + "px"
+        }, 200);
+      }
+    });
+  }
+
+})
+.directive("brotherIconDirective", function() {
+  return function(scope, element, attrs) {
+    if (scope.$last) {
+      scope.viewModel.brotherIconSize = $(".brotherIcon").height();
+      scope.viewModel.brotherIconCenters = scope.viewModel.calcIconCenters();
+    }
   }
 });
--- public/javascripts/controllers/brothers/BrotherHomeController.js
+++ public/javascripts/controllers/brothers/BrotherHomeController.js
@@ -8,17 +8,39 @@ ndapp.controller('brotherHomeController', function($scope, ndService) {
 
   var setViewModel = function() {
     $scope.viewModel.windowWidth = $(window).width();
-    $scope.$apply();
   }
 
-  var private = (function() {
-    return {
-
-    }
-  })();
+  var private = {};
+  var setPrivateVars = function() {
+    private.navigator = $("#navigator");
+    private.navBulletSize = $(".navBullet").height();
+    private.navCenters = helpers.calcNavCenters();
+  }
 
   var helpers = (function() {
+
+    /**
+     * Calculates the position of the center of
+     * an element.
+     */
+    function calcCenter(elem) {
+      var offset = elem.offset();
+      var x = offset.left + (elem.width()/2);
+      var y = offset.top + (elem.height()/2);  
+      return {x: x, y: y};
+    }
+
+    function calcNavCenters() {
+      var navCenters = {};
+      $(".navBullet").each(function(i) {
+        var center = calcCenter($(this));
+        navCenters["#navBullet" + (i+1)] = center;
+      });
+      return navCenters;
+    }
  
+    function staticSizingJS() {}
+
     function sizingJS() {}
 
     function responsiveJS(windowWidth) {
@@ -28,6 +50,9 @@ ndapp.controller('brotherHomeController', function($scope, ndService) {
     }
 
     return {
+      calcCenter: calcCenter,
+      calcNavCenters: calcNavCenters, 
+      staticSizingJS: staticSizingJS,
       sizingJS: sizingJS, 
       responsiveJS: responsiveJS
     }
@@ -36,15 +61,53 @@ ndapp.controller('brotherHomeController', function($scope, ndService) {
   var init = (function() {
     ndService.headerIntroDeferred.resolve(false);
 
+    setPrivateVars();
     setViewModel();
 
+    helpers.staticSizingJS();
     helpers.sizingJS();
     $(window).resize(function() {
       helpers.responsiveJS($(window).width());
     });
+
+    eventHandlers();
   })();
 
   function eventHandlers() {
+    function resizeNavBullet(e) {
+      private.navCenters = helpers.calcNavCenters();
+      $(".navBullet").each(function(i) {
+        var center = private.navCenters["#navBullet" + (i+1)];
+        var yDiff = Math.abs(center.y - e.pageY);
+        var maxDiff = private.navBulletSize * 1.5;
 
+        var newSize;
+
+        if (yDiff > maxDiff) {
+          newSize = private.navBulletSize;
+        } else {
+          var multiplier = ((-1/(maxDiff*2))*yDiff) + 1.5;
+          newSize = private.navBulletSize * multiplier;
+        }
+
+        $(this).height(newSize)
+               .width(newSize)
+               .css("border-radius", (newSize/2) + "px");
+      });
+    }
+
+    $("#navigator").on({
+      mouseenter: function() {
+        $(document).on("mousemove", resizeNavBullet);
+      }, 
+      mouseleave: function() {
+        $(document).off("mousemove", resizeNavBullet);
+        $(".navBullet").animate({
+          "height": private.navBulletSize, 
+          "width": private.navBulletSize, 
+          "border-radius": (private.navBulletSize/2) + "px"
+        }, 200);
+      }
+    });
   }
 });
