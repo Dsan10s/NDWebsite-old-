1adee67d2765c32cca1bc6f1928b15203fe6348e
Daniel Sanchez <dsan@mit.edu>
2014-08-04 20:41:20 -0700

    about us done

--- public/javascripts/controllers/navbar/NavBarController.js
+++ public/javascripts/controllers/navbar/NavBarController.js
@@ -3,14 +3,16 @@ var ndapp = angular.module('ndapp');
 ndapp.controller('navbarController', function($scope, ndService) {
 
   $scope.viewModel = {
-    links: undefined
+    links: undefined, 
+    contactUsText: "Contact Us", 
+    locateUsText: "Locate Us"
   };
 
   (function setViewModel() {
     var links = [{"text": "Home", "href": "/home", "icon": "home"}, 
                  {"text": "Brothers", "href": "/brothers", "icon": "user"}, 
                  {"text": "House", "href": "/house", "icon": "tower"}, 
-                 {"text": "About Us", "href": "/about", "icon": "globe"}];
+                 {"text": "About Us", "href": "#", "dataToggle": "modal", "dataTarget": "#aboutModal", "icon": "globe"}];
 
     $scope.viewModel.links = links;
   })();
@@ -38,62 +40,6 @@ ndapp.controller('navbarController', function($scope, ndService) {
     helpers.animateHeaderIntro();
   })();
 
-  function eventHandlers() {/*
-
-    var two = new Two({width: 120, height: 120}).appendTo($(".glowContainer")[0]);
-    var r1 = 49, r2 = 60, currentRadius = r1, rDiff = r2 - r1;
-    var circle = two.makeCircle(60, 60, r1);
-    var animating = false;
-
-    circle.fill = 'white';
-    circle.noStroke();
-
-    two.update();
-
-
-    function animateGlow() {
-      if (!animating) {
-        animating = true;
-        
-        two.bind('update', function animateGlowFrame(frameCount) {
-          var totalMs = 500;
-          var timeRatio = totalMs / 1000;
-
-          var totalFrames = 60 * timeRatio;
-
-          if (frameCount / 60 <= timeRatio) {
-
-            currentRadius += rDiff / totalFrames
-            circle.scale = currentRadius / r1;
-
-            var framesBeforeFade = 15;
-            if (frameCount > framesBeforeFade) {
-              circle.opacity -= 1 / (totalFrames - framesBeforeFade);
-            }
-          } else {
-            circle.scale = 1;
-            circle.opacity = 1;
-            currentRadius = r1;
-
-            animating = false;
-            two.frameCount = 0;
-            two.unbind('update', animateGlowFrame);
-            two.update();
-            two.pause();
-          }
-        }).play();
-      }
-    }
-
-    $(".phoenixIcon").on({
-      mouseenter: function() {
-        animateGlow();
-      }, 
-      click: function() {
-        animateGlow();
-      }
-    });*/
-
-  }
+  function eventHandlers() {}
 
 });
