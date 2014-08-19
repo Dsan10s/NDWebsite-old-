7bce07194cd97ba95826714b0c035b349b27091f
Daniel Sanchez <dsan@mit.edu>
2014-08-19 00:49:10 -0400

    changed date and description of laser tag, removed lofted beds description

--- public/javascripts/controllers/homepage/HomePageController.js
+++ public/javascripts/controllers/homepage/HomePageController.js
@@ -21,18 +21,6 @@ ndapp.controller('homepageController', function($scope, ndService) {
 
   var private = (function() {
     return {
-      monthArray: ["January", 
-                   "February", 
-                   "March", 
-                   "April", 
-                   "May", 
-                   "June", 
-                   "July", 
-                   "August", 
-                   "September",  
-                   "October", 
-                   "November", 
-                   "December"], 
       monthsWithEvents: [], 
       animate: true, 
       events: []
