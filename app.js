c3b4d15aed86a9884b32b09b7b8fd343b1b94954
Daniel Sanchez <dsan@mit.edu>
2014-08-28 20:56:16 -0400

    app.js changed back

--- app.js
+++ app.js
@@ -22,12 +22,11 @@ app.configure(function(){
   });
 
   app.use(function(err, req, res, next) {
-    // res.render('errors/pageNotFound', {
-    //   status: err.status || 500, 
-    //   error: err, 
-    //   title: "Page not found"
-    // });
-    res.redirect('/');
+    res.render('errors/pageNotFound', {
+      status: err.status || 500, 
+      error: err, 
+      title: "Page not found"
+    });
   });
 });
 
